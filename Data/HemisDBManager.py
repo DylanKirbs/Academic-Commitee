0394794b7c6dbfeb9d58aff462a0181c5b59bd65
Dylan <dylan.kirby.365@gmail.com>
2022-10-19 18:54:08 +0200

    Faculties into DB

--- Data/HemisDBManager.py
+++ Data/HemisDBManager.py
@@ -1,6 +1,42 @@
+from dataclasses import dataclass
 from os import path
 from shutil import copyfile
 import sqlite3
+
+@dataclass
+class faculty:
+    name: str
+
+    def __str__(self) -> str:
+        return f"{self.name}"
+
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
+@dataclass
+class module:
+    name: str
+    code: str
+    credits: faculty
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.code})"
+
+    def __repr__(self) -> str:
+        return f"{self.name} ({self.code})"
+
+@dataclass
+class degree:
+    abbreviation: str
+    name: str
+    faculty: faculty
+    modules: list[module]
+
+    def __str__(self) -> str:
+        return f"{self.name} ({self.abbreviation})"
+
+    def __repr__(self) -> str:
+        return f"{self.name} ({self.abbreviation})"
 
 class HemisDataBase():
 
@@ -26,7 +62,7 @@ class HemisDataBase():
         module_table = """CREATE TABLE IF NOT EXISTS module (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             name TEXT NOT NULL,
-            number INTEGER NOT NULL,
+            code INTEGER NOT NULL,
             credits INTEGER NOT NULL
         ); """
 
@@ -79,8 +115,66 @@ class HemisDataBase():
             raise Exception('Backup database does not exist')
 
         copyfile('Data/Backup/HemisDB.db', 'Data/HemisDB.db')
-        
+
+    def closeDB(self):
+        """ Close the database connection
+        :return: None
+        """
+        self._conn.close()
+
+    def addFaculty(self, faculty: faculty):
+        """ Add a faculty to the database
+        :param faculty: The faculty to add
+        :return: None
+        """
+        sql = f"INSERT INTO faculty (name) VALUES ('{faculty.name}');"
+        self.executeSQL(sql)
+        self.commitSQL()
+
+    def addModule(self, module: module):
+        """ Add a module to the database
+        :param module: The module to add
+        :return: None
+        """
+        sql = f"INSERT INTO module (name, code, credits) VALUES ('{module.name}', '{module.code}', '{module.credits}');"
+        self.executeSQL(sql)
+        self.commitSQL()
+
+    def addDegree(self, degree: degree):
+        """ Add a degree to the database
+        :param degree: The degree to add
+        :return: None
+        """
+        # Get the faculty id
+        sql = f"SELECT id FROM faculty WHERE name = '{degree.faculty.name}';"
+        self.executeSQL(sql)
+        faculty_id = self._c.fetchone()[0]
+
+        # Get the module ids
+        module_ids = []
+        for module in degree.modules:
+            sql = f"SELECT id FROM module WHERE name = '{module.name}' AND code = '{module.code}';"
+            self.executeSQL(sql)
+            module_ids.append(self._c.fetchone()[0])
+
+        sql = f"INSERT INTO degree (abbreviation, name, faculty_id, module_ids) VALUES ('{degree.abbreviation}', '{degree.name}', '{faculty_id}', '{module_ids}');"
+        self.executeSQL(sql)
+        self.commitSQL()
+
+    def getFaculties(self):
+        """ Get all the faculties from the database
+        :return: A list of faculties
+        """
+        sql = "SELECT * FROM faculty;"
+        self.executeSQL(sql)
+        faculties = self._c.fetchall()
+        faculties = list(faculty(f[1]) for f in faculties)
+        return faculties
 
 if __name__ == "__main__":
     # Test the database
     db = HemisDataBase()
+
+    print(db.getFaculties())
+
+    db.closeDB()
